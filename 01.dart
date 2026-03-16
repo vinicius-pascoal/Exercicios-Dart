2ad07f604d458a750d7b2b6957ada09244bc74ce
Vinicius <viniciuspascoal013@gmail.com>
2026-03-16 08:07:15 -0300

    questão 1

--- 01.dart
+++ 01.dart
@@ -1,1 +1,21 @@
-// TODO
+import 'dart:io';
+
+void main() {
+  const boloPrecos = {'ovos': 5.0, 'chocolate': 7.0, 'cenoura': 6.0};
+  print("opções de bolo: ovos, chocolate, cenoura");
+  var total = 0.0;
+  while (true) {
+    print("Digite o nome do bolo (ou 'sair' para encerrar):");
+    String? bolo = stdin.readLineSync();
+    if (bolo == 'sair') {
+      break;
+    }
+    if (boloPrecos.containsKey(bolo)) {
+      total += boloPrecos[bolo]!;
+      print("bolo adicionado");
+    } else {
+      print("Desculpe, não temos o bolo de $bolo.");
+    }
+  }
+  print("Total:$total");
+}
