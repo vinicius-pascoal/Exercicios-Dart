adcdc5728a65a1022d3035eeb61997609b6ec023
Vinicius <viniciuspascoal013@gmail.com>
2026-03-23 07:41:32 -0300

    Merge and sort two integer vectors
    Replace the TODO with a Dart program that reads two space-separated integer vectors from stdin, concatenates them into a third list, sorts the combined list, and prints the result. Adds dart:io import and simple prompts; author comment included.

--- 14.dart
+++ 14.dart
@@ -1,1 +1,17 @@
-// TODO
+// Vinicius Pascoal
+import 'dart:io';
+
+void main() {
+  print('Digite os elementos do primeiro vetor (separados por espaço):');
+  List<int> vetor1 = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+
+  print('Digite os elementos do segundo vetor (separados por espaço):');
+  List<int> vetor2 = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+
+  List<int> vetor3 = []
+    ..addAll(vetor1)
+    ..addAll(vetor2);
+  vetor3.sort();
+
+  print('Vetor 3: $vetor3');
+}
