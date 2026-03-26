e980b3e9b0d61511ff528f6565b00eeeb9770e86
Ms-footsteps <tayssapassoss@gmail.com>
2026-03-25 22:51:32 -0300

    add: media turma

--- 04.dart
+++ 04.dart
@@ -1,10 +1,11 @@
 //Tayssa Passos
 import 'dart:io';
-import 'dart:math';
 
 void main(List<String> args) {
   var alunos = <Map<String, dynamic>>[];
   double mediaTurma = 0.0;
+  String situacao = "";
+  double totalMedias = 0.0;
 
   print(
     "Para cada aluno insira a matrícula, nome, sexo ('M’ e 'F'), três notas e o número de faltas\n Inserir 00000 na matricula encerra o programa.",
@@ -26,9 +27,17 @@ void main(List<String> args) {
         stdout.write("Nota ${j + 1}: ");
         notasAlunoX.add(double.parse(stdin.readLineSync()!));
       }
+      double media = notasAlunoX.reduce((a, b) => a + b) / 3;
+      totalMedias += media;
 
       stdout.write("Insira a quantidade de faltas do aluno: ");
       int faltas = int.parse(stdin.readLineSync()!);
+
+      if (media >= 7 && faltas <= 18) {
+        situacao = "Aprovado";
+      } else {
+        situacao = "Reprovado";
+      }
 
       alunos.add({
         "matricula": matricula,
@@ -36,9 +45,13 @@ void main(List<String> args) {
         "sexo": sexo,
         "notas": notasAlunoX,
         "faltas": faltas,
+        "media": media,
+        "situacao": situacao,
       });
     }
   }
+
+  mediaTurma = totalMedias / alunos.length;
 
   print(alunos);
 }
