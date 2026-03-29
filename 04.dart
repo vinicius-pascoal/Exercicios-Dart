43f9cc45c3f8d39e3ac7cf35191a88b37406895e
Ms-footsteps <tayssapassoss@gmail.com>
2026-03-29 10:27:20 -0300

    questao 04

--- 04.dart
+++ 04.dart
@@ -4,8 +4,17 @@ import 'dart:io';
 void main(List<String> args) {
   var alunos = <Map<String, dynamic>>[];
   double mediaTurma = 0.0;
+  double mediaFem = 0.0;
+  double percentualAprovados = 0.0;
+  String maiorMediaM = "00000";
+  String maiorMediaF = "00000";
+  double maiorMediaValorM = -1;
+  double maiorMediaValorF = -1;
   String situacao = "";
   double totalMedias = 0.0;
+  int alunosAprovados = 0;
+  int sexoFeminino = 0;
+  double totalMediasFem = 0.0;
 
   print(
     "Para cada aluno insira a matrícula, nome, sexo ('M’ e 'F'), três notas e o número de faltas\n Inserir 00000 na matricula encerra o programa.",
@@ -21,6 +30,7 @@ void main(List<String> args) {
 
       stdout.write("Insira o sexo do aluno (F ou M): ");
       String? sexo = stdin.readLineSync()!;
+      sexo = sexo.toUpperCase();
 
       List<double> notasAlunoX = [];
       for (int j = 0; j < 3; j++) {
@@ -29,12 +39,24 @@ void main(List<String> args) {
       }
       double media = notasAlunoX.reduce((a, b) => a + b) / 3;
       totalMedias += media;
+      if (sexo == "F") {
+        sexoFeminino += 1;
+        totalMediasFem += media;
+      }
 
       stdout.write("Insira a quantidade de faltas do aluno: ");
       int faltas = int.parse(stdin.readLineSync()!);
 
       if (media >= 7 && faltas <= 18) {
         situacao = "Aprovado";
+        alunosAprovados++;
+        if (sexo == "M" && media > maiorMediaValorM) {
+          maiorMediaValorM = media;
+          maiorMediaM = matricula;
+        } else if (sexo == "F" && media > maiorMediaValorF) {
+          maiorMediaValorF = media;
+          maiorMediaF = matricula;
+        }
       } else {
         situacao = "Reprovado";
       }
@@ -51,7 +73,35 @@ void main(List<String> args) {
     }
   }
 
-  mediaTurma = totalMedias / alunos.length;
+  if (alunos.isNotEmpty) {
+    mediaTurma = totalMedias / alunos.length;
+    percentualAprovados = (alunosAprovados / alunos.length) * 100;
+  } else {
+    print("Nenhum aluno cadastrado.");
+    return;
+  }
 
-  print(alunos);
+  if (sexoFeminino > 0) {
+    mediaFem = totalMediasFem / sexoFeminino;
+  } else {
+    mediaFem = 0;
+  }
+
+  print("A média da turma é: ${mediaTurma.toStringAsFixed(2)}");
+  print(
+    "O percentual de alunos aprovados é: ${percentualAprovados.toStringAsFixed(2)}%",
+  );
+
+  if (maiorMediaValorM == -1) {
+    print("Nenhum aluno do sexo masculino aprovado.");
+  } else {
+    print("Matrícula do aluno do sexo masculino com maior média: $maiorMediaM");
+  }
+  if (maiorMediaValorF == -1) {
+    print("Nenhum aluno do sexo feminino aprovado.");
+  } else {
+    print("Matrícula do aluno do sexo feminino com maior média: $maiorMediaF");
+  }
+
+  print("A média das alunas é: ${mediaFem.toStringAsFixed(2)}");
 }
